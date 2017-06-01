25073fd0e28184eb4ac0654302a634e1f8e46bbb
anz <anz.factory@gmail.com>
2017-06-02 08:49:25 +0900

    サンプルゲームとしてミニゲーいれてみた
    使い方の流れとかほんわり参考になればいいなーぐらいのイメージ

---
+++ Assets/Sample/Game/ScoreCanvas.cs
@@ -0,0 +1,56 @@
+﻿/*********************************
+ Score Canvas
+*********************************/
+using UnityEngine;
+using Xyz.Anzfactory.NCMBUtil;
+
+namespace Sample.Game
+{
+
+    public class ScoreCanvas : MonoBehaviour
+    {
+        #region "Serialize Fields"
+        [SerializeField] private RankingBoard rankingBoard;
+        #endregion
+
+        #region "Fields"
+        #endregion
+
+        #region "Properties"
+        #endregion
+
+        #region "Events"
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+        }
+
+        private void Update()
+        {
+        }
+
+        private void FixedUpdate()
+        {
+        }
+
+        private void OnDestroy()
+        {
+        }
+
+        #endregion
+
+        #region "Public Methods"
+        public void Show()
+        {
+            this.rankingBoard.Show();
+        }
+        #endregion
+
+        #region "Private Methods"
+        #endregion
+    }
+
+}
